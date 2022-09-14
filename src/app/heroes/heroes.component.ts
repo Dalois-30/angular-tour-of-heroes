b56ef41379480a2519f00f1341c4ad1ee7ec80e8
Dalois-30 <claudetapi@yahoo.com>
2022-09-14 23:46:13 +0100

    ajout du service message et debut du routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   Heroes: Hero[] = []
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes()
@@ -19,12 +20,13 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`);
     console.log(hero);
     
   }
 
   getHeroes(): void {
-    this.Heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.Heroes = heroes);
   }
 
 }
